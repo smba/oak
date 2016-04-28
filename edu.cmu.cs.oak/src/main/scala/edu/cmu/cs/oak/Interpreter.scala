a55ce27b3e694c13386dfafb200450a00b0152e6
Stefan <you@example.com>
2016-04-28 18:25:31 -0400

    basic idea

--- edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/Interpreter.scala
+++ edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/Interpreter.scala
@@ -1,12 +1,22 @@
 package edu.cmu.cs.oak
 
-import com.caucho.quercus.script.QuercusScriptEngineFactory
+import com.caucho.quercus.program.QuercusProgram
+import com.caucho.quercus.statement.BlockStatement
 
-object Interpreter extends App {
-  val factory = new QuercusScriptEngineFactory();
-  val engine = factory.getScriptEngine();
-  
-  val code = "<?php $foo = strlen('abc'); print $foo; return 'yikes'; ?>";
-  val o = engine.eval(code);
-  System.out.println(o);
+object Interpreter {
+
+  def eval(element: AnyRef): String = element match {
+
+    /**
+     * Execution for BlockStatements 
+     */
+    case s: BlockStatement => {
+      var res = ""
+      s.getStatements.foreach {
+        stmt => res += eval(stmt)
+      }
+      res
+    }
+  }
+
 }
