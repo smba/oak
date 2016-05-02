8fb90f15108974eca1e033c194ab6603d7c0813c
Stefan <you@example.com>
2016-05-02 12:03:32 -0400

    syntactic sugar for BooleanValues

--- edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/env/BooleanValue.scala
+++ edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/env/BooleanValue.scala
@@ -2,4 +2,10 @@ package edu.cmu.cs.oak.env
 
 case class BooleanValue(v:Boolean) extends OakValue {
   
+  // Syntactic sugar
+  def &&(v2:BooleanValue): BooleanValue = BooleanValue(v && v2.value)
+  def ||(v2:BooleanValue): BooleanValue = BooleanValue(v || v2.value)
+  def not(): BooleanValue = BooleanValue(!v)
+  
+  def value = v
 }
