a7733431af26bc17f01818155bd9402bf141c2ee
Stefan <smuhlbau@andrew.cmu.edu>
2016-05-02 18:39:28 -0400

    update

--- project/Build.scala
+++ project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object OakBuild extends Build {
 	lazy val quercus = Project(id = "quercus", base = file("com.caucho.quercus"))
-	lazy val oak = Project(id = "oak", base = file("edu.cmu.cs.oak")) dependsOn(quercus)
+	lazy val oak = Project(id = "oak", base = file("edu.cmu.cs.oak")) dependsOn(quercus) aggregate(quercus)
 	lazy val symex = Project(id = "symex", base = file("edu.iastate.symex")) dependsOn(phpparser, featureexprlib)
 	lazy val phpparser = Project(id = "phpparser", base = file("org.eclipse.php"))
 	lazy val featureexprlib = Project(id = "featureexpr", base = file("de.fosd.typechef.featureexpr"))
