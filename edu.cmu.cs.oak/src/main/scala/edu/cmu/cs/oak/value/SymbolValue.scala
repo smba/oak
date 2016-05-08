b26b729fe8f4b24a75602acbf7f7c3cce918e5c4
Stefan <smuhlbau@andrew.cmu.edu>
2016-05-08 05:03:48 -0400

    update

--- edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/value/SymbolValue.scala
+++ edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/value/SymbolValue.scala
@@ -6,6 +6,6 @@ import com.caucho.quercus.expr.Expr
   *
   */
 case class SymbolValue(e: Expr, id: Long) extends SymbolicValue {
-  System.err.println(">>> Symbol Σ["+id+"] introduced for expression " + e +".")
+  //System.err.println(">>> Symbol Σ["+id+"] introduced for expression " + e +".")
   override def toString = "Σ["+id+"]"
 }
