f5602983a59dedea2cd09e7da733510e4bcb3cbc
Stefan <smuhlbau@andrew.cmu.edu>
2016-05-12 18:34:30 -0400

    added classDef

---
+++ edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/value/ClassDef.scala
@@ -0,0 +1,27 @@
+package edu.cmu.cs.oak.value
+
+case class ClassDef(name: String, fields: List[String], methods: Map[String, FunctionDef]) extends OakValue {
+  
+  /**
+   * Constructors
+   */
+  var constructors = Map[Int, FunctionDef]()
+  
+  def addConstructor(i: Int, fd: FunctionDef): Unit = {
+    constructors += (i -> fd)
+  }
+  
+  def getConstructor(i: Int): FunctionDef = {
+    try {
+      constructors.get(i).get
+    } catch {
+      case nsee: NoSuchElementException => throw new RuntimeException("Constructor for " + name + " with " + i + " arguments not found (" + constructors.size + " constructor(s) available).")
+    }
+  }
+  
+  
+  def getName(): String = name
+  def getMethods(fname: String): FunctionDef = methods.get(fname).get
+  def getFields(): List[String] = fields
+  
+}
