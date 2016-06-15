792b13ee3dca02d5a7fcf61244257225300f3f49
Stefan <smuhlbau@andrew.cmu.edu>
2016-06-15 18:37:46 -0400

    Class Simpleenv will be removed

--- edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/env/SimpleEnv.scala
+++ edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/env/SimpleEnv.scala
@@ -2,6 +2,6 @@ package edu.cmu.cs.oak.env
 
 import scala.collection.immutable.Stack
 
-class SimpleEnv(parent: EnvListener, calls: Stack[String], constraint: String) extends Environment(parent: EnvListener, calls: Stack[String], constraint: String) {
+@deprecated class SimpleEnv(parent: EnvListener, calls: Stack[String], constraint: String) extends Environment(parent: EnvListener, calls: Stack[String], constraint: String) {
   
 }
