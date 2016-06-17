bcd4d8b933c7224e21a3eb5279b21af6f3c14975
Stefan <smuhlbau@andrew.cmu.edu>
2016-06-17 19:02:28 -0400

    removed build dependency

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ object OakBuild extends Build {
 	lazy val kernel = Project(id = "kernel", base = file("kernel")) dependsOn(hessian)
 	lazy val quercus = Project(id = "quercus", base = file("quercus")) dependsOn(kernel)
 	lazy val oak = Project(id = "oak", base = file("edu.cmu.cs.oak")) dependsOn(quercus)
-	lazy val symex = Project(id = "symex", base = file("edu.iastate.symex")) dependsOn(phpparser, featureexprlib)
+	lazy val symex = Project(id = "symex", base = file("edu.iastate.symex")) dependsOn(phpparser)
 	lazy val phpparser = Project(id = "phpparser", base = file("org.eclipse.php"))
-	lazy val featureexprlib = Project(id = "featureexpr", base = file("de.fosd.typechef.featureexpr"))
-	lazy val difftest = Project(id = "difftest", base = file("edu.cmu.cs.oak.difftest"))
+	//lazy val featureexprlib = Project(id = "featureexpr", base = file("de.fosd.typechef.featureexpr"))
+	//lazy val difftest = Project(id = "difftest", base = file("edu.cmu.cs.oak.difftest"))
 }
