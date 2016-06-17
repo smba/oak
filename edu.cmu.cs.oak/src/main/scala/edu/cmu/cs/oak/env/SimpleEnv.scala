a5d4d55ac30aafcfc966865fdf19ff776e6a4f6f
Stefan <smuhlbau@andrew.cmu.edu>
2016-06-17 18:59:53 -0400

    build.sbt
    added featureexprlib dependency

--- edu.cmu.cs.oak/src/main/scala/edu/cmu/cs/oak/env/SimpleEnv.scala
+++
@@ -1,7 +0,0 @@
-package edu.cmu.cs.oak.env
-
-import scala.collection.immutable.Stack
-
-@deprecated class SimpleEnv(parent: EnvListener, calls: Stack[String], constraint: String) extends Environment(parent: EnvListener, calls: Stack[String], constraint: String) {
-  
-}
