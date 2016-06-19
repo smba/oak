34f5ff0ca55574a9aea53e9377f4b8d48e89d4c1
Stefan <smuhlbau@andrew.cmu.edu>
2016-06-18 21:48:32 -0400

    added builtin classes for PHP

---
+++ edu.cmu.cs.oak/src/main/resources/COM.php
@@ -0,0 +1,19 @@
+<?php
+class COM {
+	public function __construct($module_name) {
+	}
+	public function AddRef() {
+	}
+	public function Release() {
+	}
+	public function All() {
+	}
+	public function Next() {
+	}
+	public function Prev() {
+	}
+	public function Reset() {
+	}
+}
+
+?>
