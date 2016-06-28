1363ea76486a776c0e8b583db2e14d8b72b58b52
Stefan <smuhlbau@andrew.cmu.edu>
2016-06-28 19:43:18 -0400

    added addressbook

---
+++ edu.cmu.cs.oak/src/main/resources/php_user_filter.php
@@ -0,0 +1,14 @@
+<?php
+class php_user_filter {
+	/* Properties */
+	public $filtername;
+	public $params;
+	/* Methods */
+	public function filter($in, $out, &$consumed, $closing) {
+	}
+	public function onClose() {
+	}
+	public function onCreate() {
+	}
+}
+?>
